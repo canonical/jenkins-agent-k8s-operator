a9d97b7f8b56c0f4e6eda2fa72281fd7bddaa471
Tom Haddon <tom@haddons.net>
2020-07-21 13:27:19 +0100

    Add tox and Makefile targets, and initial unit test

---
+++ tests/unit/test_charm.py
@@ -0,0 +1,19 @@
+import unittest
+
+import sys
+
+sys.path.append('lib')  # noqa: E402
+sys.path.append('src')  # noqa: E402
+
+from charm import generate_pod_config
+
+
+class TestJenkinsAgentCharm(unittest.TestCase):
+    def test_generate_pod_config(self):
+        config = {
+            "jenkins_user": "test",
+        }
+        expected = {
+            "JENKINS_API_USER": "test",
+        }
+        self.assertEqual(generate_pod_config(config), expected)
