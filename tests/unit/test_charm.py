2d4df32d5e87eabfba58480e58a7ab46c17ff149
alejdg <alexandre.gomes@canonical.com>
2020-08-12 15:02:13 -0700

    Add unit tests

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -3,19 +3,132 @@
 
 import unittest
 
+from ops import testing
+from ops.model import ActiveStatus, BlockedStatus
+
+
 import sys
 
 sys.path.append('src')  # noqa: E402
 
-from charm import generate_pod_config
+from charm import JenkinsAgentCharm
+
+CONFIG_DEFAULT = {
+    "image": "jenkins-agent-operator",
+    "jenkins_master_url": "",
+    "jenkins_agent_name": "",
+    "jenkins_agent_token": ""
+}
+
+CONFIG_ONE_AGENT = {
+    "jenkins_master_url": "http://test",
+    "jenkins_agent_name": "agent-one",
+    "jenkins_agent_token": "token-one"
+}
+
+CONFIG_ONE_AGENT_CUSTOM_IMAGE = {
+    "image": "image-name",
+    "jenkins_master_url": "http://test",
+    "jenkins_agent_name": "agent-one",
+    "jenkins_agent_token": "token-one"
+}
+
+SPEC_EXPECTED = {
+    'containers': [{
+        'config': {
+            'JENKINS_AGENTS': 'agent-one',
+            'JENKINS_TOKENS': 'token-one',
+            'JENKINS_URL': 'http://test'
+        },
+        'imageDetails': {
+            'imagePath': 'image-name'
+        },
+        'name': 'jenkins-agent',
+        'readinessProbe': {
+            'exec': {
+                'command': [
+                    '/bin/cat',
+                    '/var/lib/jenkins/agents/.ready'
+                ]
+            }
+        }
+    }]
+}
 
 
 class TestJenkinsAgentCharm(unittest.TestCase):
-    def test_generate_pod_config(self):
-        config = {
-            "jenkins_user": "test",
-        }
+    def setUp(self):
+        self.harness = testing.Harness(JenkinsAgentCharm)
+        self.harness.begin()
+        self.harness.disable_hooks()
+        self.harness.update_config(CONFIG_DEFAULT)
+
+    def test__generate_pod_config(self):
         expected = {
-            "JENKINS_API_USER": "test",
+            "JENKINS_URL": "http://test",
         }
-        self.assertEqual(generate_pod_config(config), expected)
+        self.assertEqual(self.harness.charm.generate_pod_config(CONFIG_ONE_AGENT, secured=True), expected)
+
+        expected["JENKINS_AGENTS"] = "agent-one"
+        expected["JENKINS_TOKENS"] = "token-one"
+        self.assertEqual(self.harness.charm.generate_pod_config(CONFIG_ONE_AGENT, secured=False), expected)
+
+    def test__configure_pod__invalid__config(self):
+        """Test configure_pod when the config is invalid"""
+        self.harness.charm.on.config_changed.emit()
+        message = "Missing required config: jenkins_master_url jenkins_agent_name jenkins_agent_token"
+        self.assertEqual(self.harness.model.unit.status, BlockedStatus(message))
+        self.assertEqual(self.harness.get_pod_spec(), None)
+
+    def test__configure_pod__unit__not__leader(self):
+        """Test configure_pod when the unit isn't leader"""
+        self.harness.set_leader(is_leader=False)
+        self.harness.update_config(CONFIG_ONE_AGENT)
+        with self.assertLogs(level='INFO') as logger:
+            self.harness.charm.on.config_changed.emit()
+            self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+            message = "INFO:root:Spec changes ignored by non-leader"
+            self.assertEqual(logger.output[-1], message)
+            self.assertEqual(self.harness.get_pod_spec(), None)
+
+    def test__configure_pod(self):
+        """Test configure_pod"""
+        expected = (SPEC_EXPECTED, None)
+
+        self.harness.set_leader(is_leader=True)
+        self.harness.update_config(CONFIG_ONE_AGENT_CUSTOM_IMAGE)
+        with self.assertLogs(level='INFO') as logger:
+            print(self.harness.get_pod_spec())
+            self.harness.charm.on.config_changed.emit()
+            self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+            self.assertEqual(self.harness.get_pod_spec(), expected)
+
+    def test__configure_pod__no__spec_change(self):
+        """Test configure_pod when there is no change in the spec"""
+        self.harness.set_leader(is_leader=True)
+        self.harness.update_config(CONFIG_ONE_AGENT_CUSTOM_IMAGE)
+        self.harness.charm._stored._spec = self.harness.charm.make_pod_spec()
+        with self.assertLogs(level='INFO') as logger:
+            self.harness.charm.on.config_changed.emit()
+            self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+            print(logger.output[-1])
+            message = "INFO:root:Pod spec unchanged"
+            self.assertEqual(logger.output[-1], message)
+            self.assertEqual(self.harness.get_pod_spec(), None)
+
+    def test__make_pod_spec(self):
+        """Test the construction of the spec based on juju config"""
+        self.harness.update_config(CONFIG_ONE_AGENT_CUSTOM_IMAGE)
+        self.assertEqual(self.harness.charm.make_pod_spec(), SPEC_EXPECTED)
+
+    def test__is_valid_config(self):
+        """Test config validation"""
+        config = {
+            "image": "image-name",
+            "jenkins_master_url": "http://test",
+            "jenkins_agent_name": "agent-one",
+            "jenkins_agent_token": "token-one"
+        }
+        self.assertEqual(self.harness.charm.is_valid_config(), False)
+        self.harness.update_config(config)
+        self.assertEqual(self.harness.charm.is_valid_config(), True)
