229c30a7f10fcc12df66f47c5405e70cfd4dbc6c
jdkandersson <david.andersson@canonical.com>
2022-08-22 14:00:54 +1000

    Refactor: use pebble to replan services instead of manually retsrating the container
    - Add IS charms team to maintainer list
    - Change pyproject.toml file to be in line with template
    - Get unit to enter maintenance status when making changes to the container and use replan_services instead of manually restarting the container

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -23,9 +23,9 @@ def test__get_env_config_initial(harness: Harness[JenkinsAgentCharm]):
     env_config = harness.charm._get_env_config()
 
     assert env_config == {
-        'JENKINS_AGENTS': '',
-        'JENKINS_TOKENS': '',
-        'JENKINS_URL': '',
+        "JENKINS_AGENTS": "",
+        "JENKINS_TOKENS": "",
+        "JENKINS_URL": "",
     }
 
 
@@ -49,9 +49,9 @@ def test__get_env_config_config(harness: Harness[JenkinsAgentCharm]):
     env_config = harness.charm._get_env_config()
 
     assert env_config == {
-        'JENKINS_AGENTS': jenkins_agent_name,
-        'JENKINS_TOKENS': jenkins_agent_token,
-        'JENKINS_URL': jenkins_url,
+        "JENKINS_AGENTS": jenkins_agent_name,
+        "JENKINS_TOKENS": jenkins_agent_token,
+        "JENKINS_URL": jenkins_url,
     }
 
 
@@ -90,9 +90,9 @@ def test__get_env_config_relation(
     env_config = harness.charm._get_env_config()
 
     assert env_config == {
-        'JENKINS_AGENTS': expected_jenkins_agent_name,
-        'JENKINS_TOKENS': expected_jenkins_agent_token,
-        'JENKINS_URL': jenkins_url,
+        "JENKINS_AGENTS": expected_jenkins_agent_name,
+        "JENKINS_TOKENS": expected_jenkins_agent_token,
+        "JENKINS_URL": jenkins_url,
     }
 
 
@@ -126,9 +126,9 @@ def test__get_env_config_config_relation(harness: Harness[JenkinsAgentCharm]):
     env_config = harness.charm._get_env_config()
 
     assert env_config == {
-        'JENKINS_AGENTS': relation_jenkins_agent_name,
-        'JENKINS_TOKENS': relation_jenkins_agent_token,
-        'JENKINS_URL': relation_jenkins_url,
+        "JENKINS_AGENTS": relation_jenkins_agent_name,
+        "JENKINS_TOKENS": relation_jenkins_agent_token,
+        "JENKINS_URL": relation_jenkins_url,
     }
 
 
@@ -156,67 +156,74 @@ def test_config_changed(
     arrange: given charm in its initial state with valid configuration
     act: when the config_changed event occurs
     assert: then the charm is in the active status, the container has the jenkins-agent service and
-        has been restarted and a log message indicating a layer has been added is written.
+        has been replanned and a log message indicating a layer has been added is written.
     """
     harness_pebble_ready.update_config(valid_config)
-    # Mock the restart function on the container
+    # Mock the replan_services function on the container
     container: Container = harness_pebble_ready.model.unit.get_container(
         harness_pebble_ready.charm.service_name
     )
-    mock_restart = mock.MagicMock()
-    monkeypatch.setattr(container, "restart", mock_restart)
+    mock_replan_services = mock.MagicMock()
+    monkeypatch.setattr(container.pebble, "replan_services", mock_replan_services)
 
     caplog.set_level(logging.DEBUG)
     harness_pebble_ready.charm.on.config_changed.emit()
 
     assert isinstance(harness_pebble_ready.model.unit.status, ActiveStatus)
     assert harness_pebble_ready.charm.service_name in container.get_plan().services
-    mock_restart.assert_called_once_with(harness_pebble_ready.charm.service_name)
+    mock_replan_services.assert_called_once_with()
     assert "add_layer" in caplog.text.lower()
 
 
-def test_config_changed_pebble_not_ready(harness: Harness[JenkinsAgentCharm], valid_config):
+def test_config_changed_pebble_not_ready(
+    harness: Harness[JenkinsAgentCharm],
+    valid_config,
+    monkeypatch: pytest.MonkeyPatch,
+):
     """
     arrange: given charm where the pebble is not ready state with valid configuration
     act: when the config_changed event occurs
-    assert: then the unit stayis in maintenance status and the container is not restarted.
+    assert: then the unit stayis in maintenance status and the container is not replanned.
     """
     harness.update_config(valid_config)
-    # Mock the restart function on the container
+    # Mock the replan_services function on the container
     container: Container = harness.model.unit.get_container(harness.charm.service_name)
-    container.restart = mock.MagicMock()
+    mock_replan_services = mock.MagicMock()
+    monkeypatch.setattr(container.pebble, "replan_services", mock_replan_services)
 
     harness.charm.on.config_changed.emit()
 
     assert isinstance(harness.model.unit.status, MaintenanceStatus)
-    container.restart.assert_not_called()
+    mock_replan_services.assert_not_called()
 
 
 def test_config_changed_no_change(
     harness_pebble_ready: Harness[JenkinsAgentCharm],
     valid_config,
     caplog: pytest.LogCaptureFixture,
+    monkeypatch: pytest.MonkeyPatch,
 ):
     """
     arrange: given charm in active state with valid configuration
     act: when the config_changed event occurs
-    assert: the charm stays in the active status, the container is not restarted and a log message
+    assert: the charm stays in the active status, the container is not replanned and a log message
         indicating unchaged configuration is written.
     """
     # Get container into active state
     harness_pebble_ready.update_config(valid_config)
     harness_pebble_ready.charm.on.config_changed.emit()
-    # Mock the restart function on the container
+    # Mock the replan_services function on the container
     container: Container = harness_pebble_ready.model.unit.get_container(
         harness_pebble_ready.charm.service_name
     )
-    container.restart = mock.MagicMock()
+    mock_replan_services = mock.MagicMock()
+    monkeypatch.setattr(container.pebble, "replan_services", mock_replan_services)
 
     caplog.set_level(logging.DEBUG)
     harness_pebble_ready.charm.on.config_changed.emit()
 
     assert isinstance(harness_pebble_ready.model.unit.status, ActiveStatus)
-    container.restart.assert_not_called()
+    mock_replan_services.assert_not_called()
     assert "unchanged" in caplog.text.lower()
 
 
@@ -334,9 +341,9 @@ def test_on_agent_relation_joined(
     harness.add_relation_unit(relation_id=relation_id, remote_unit_name=unit_name)
 
     assert harness.get_relation_data(relation_id=relation_id, app_or_unit=unit_name) == {
-        'executors': str(cpu_count),
-        'labels': machine_architecture,
-        'slavehost': unit_name.replace("/", "-"),
+        "executors": str(cpu_count),
+        "labels": machine_architecture,
+        "slavehost": unit_name.replace("/", "-"),
     }
     assert "relation" in caplog.text.lower()
     assert "joined" in caplog.text.lower()
