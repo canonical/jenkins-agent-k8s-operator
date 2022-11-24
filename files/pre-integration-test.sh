594b877f207ab1d188d1c3fd0fa35d617cbbb1dd
Andrew Liaw <yhaliaw@example.com>
2022-11-24 12:44:58 +0800

    Fix the name of lxd controller.

--- files/pre-integration-test.sh
+++ files/pre-integration-test.sh
@@ -4,7 +4,8 @@
 # https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
 
 # lxd should be install and init by a previous step in integration test action.
-juju bootstrap lxd local
-pip3 install selenium pyyaml
+juju bootstrap lxd localhost
+
+pip3 install pyyaml
 echo "Deploying jenkins"
 python3 tests/integration/deploy_jenkins.py
