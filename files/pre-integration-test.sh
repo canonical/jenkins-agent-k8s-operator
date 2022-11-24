33e657ab7227f3263f9e81e7b897d50871491350
Andrew Liaw <yhaliaw@example.com>
2022-11-24 10:31:16 +0800

    Add juju bootstrap of lxd.

--- files/pre-integration-test.sh
+++ files/pre-integration-test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo snap install lxd
-lxd init --auto
+# Pre-run script for integration test operator-workflows action.
+# https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
+
+# lxd should be install and init by a previous step in integration test action.
+juju bootstrap lxd local
 pip3 install selenium pyyaml
 echo "Deploying jenkins"
 python3 tests/integration/deploy_jenkins.py
