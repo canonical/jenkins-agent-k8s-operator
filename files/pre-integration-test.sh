106cfc370aa18f723d8f3eaa993d976a421047de
Andrew Liaw <yhaliaw@example.com>
2022-11-25 15:39:31 +0800

    Update the commands.

--- files/pre-integration-test.sh
+++ files/pre-integration-test.sh
@@ -9,14 +9,14 @@
 
 # lxd should be install and init by a previous step in integration test action.
 echo "bootstraping lxd juju controller"
-/usr/bin/sg microk8s -c microk8s status --wait-ready
-/usr/bin/sg microk8s -c juju bootstrap localhost localhost
+sg microk8s -c "microk8s status --wait-ready"
+sg microk8s -c "juju bootstrap localhost localhost"
 
 echo "Deploying jenkins"
-/usr/bin/sg microk8s -c python3 tests/integration/deploy_jenkins.py
+sg microk8s -c "python3 tests/integration/deploy_jenkins.py"
 
 echo "bootstraping microk8s juju controller"
-/usr/bin/sg microk8s -c juju bootstrap microk8s micro
-juju add-model testing -c micro
-juju switch micro:admin/testing
-sudo microk8s status --wait-ready
+sg microk8s -c "juju bootstrap microk8s micro"
+sg microk8s -c "juju add-model testing -c micro"
+sg microk8s -c "juju switch micro:admin/testing"
+sg microk8s -c "microk8s status --wait-ready"
