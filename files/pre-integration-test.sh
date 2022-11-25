f26436b34d9bf721b5f81ebaf1d32a24a8ba4524
Andrew Liaw <yhaliaw@example.com>
2022-11-25 10:14:42 +0800

    Add logging.

--- files/pre-integration-test.sh
+++ files/pre-integration-test.sh
@@ -4,11 +4,17 @@
 # https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
 
 # Enure setup of microk8s.
-sudo usermod -a -G microk8s $USER
-sudo chown -f -R $USER ~/.kube
+sudo microk8s status --wait-ready
+sudo usermod -a -G microk8s "$USER"
+sudo chown -f -R "$USER" ~/.kube
 newgrp microk8s
 
+sudo microk8s.kubectl -n kube-system rollout status -w deployment/hostpath-provisioner
+sudo microk8s.kubectl -n kube-system rollout status -w deployment/coredns
+sudo microk8s.kubectl -n container-registry rollout status -w deployment/registry
+
 # lxd should be install and init by a previous step in integration test action.
+echo "bootstraping lxd juju controller"
 juju bootstrap localhost localhost
 
 echo "Deploying jenkins"
