b499224c8f7b4d2ba24ec755a450c0b576af51ac
Andrew Liaw <yhaliaw@example.com>
2022-11-25 08:53:50 +0800

    Update to pre-integration-test.sh to setup microk8s due to occasional test failures.

--- files/pre-integration-test.sh
+++ files/pre-integration-test.sh
@@ -9,3 +9,8 @@ juju bootstrap lxd localhost
 pip3 install pyyaml
 echo "Deploying jenkins"
 python3 tests/integration/deploy_jenkins.py
+
+# Integration test sometimes fails fails due to microk8s not setup and asked to run the following.
+sudo usermod -a -G microk8s runner
+sudo chown -f -R runner ~/.kube
+newgrp microk8s
