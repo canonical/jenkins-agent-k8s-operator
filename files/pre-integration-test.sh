13ad17a324905fae2174a550801815f0b180a3b7
Andrew Liaw <yhaliaw@example.com>
2022-11-25 09:13:11 +0800

    Update pre-integration-test.sh to ensure proper setup of microk8s.

--- files/pre-integration-test.sh
+++ files/pre-integration-test.sh
@@ -6,11 +6,21 @@
 # lxd should be install and init by a previous step in integration test action.
 juju bootstrap lxd localhost
 
-pip3 install pyyaml
+# Ensure microk8s is setup correctly
+sudo microk8s status --wait-ready
+sudo microk8s enable dns hostpath-storage registry
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+
+# Ensure the features are ready
+sudo microk8s.kubectl -n kube-system rollout status -w deployment/hostpath-provisioner
+sudo microk8s.kubectl -n kube-system rollout status -w deployment/coredns
+sudo microk8s.kubectl -n container-registry rollout status -w deployment/registry
+
+# Bootstrap a juju controller on microk8s
+sg microk8s -c "juju bootstrap microk8s micro"
+juju switch micro
+
+pip3 install pyyaml python3.10
 echo "Deploying jenkins"
 python3 tests/integration/deploy_jenkins.py
-
-# Integration test sometimes fails fails due to microk8s not setup and asked to run the following.
-sudo usermod -a -G microk8s runner
-sudo chown -f -R runner ~/.kube
-newgrp microk8s
