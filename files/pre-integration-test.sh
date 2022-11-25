9678720976d1ee9b3a9191ff23e9fd89fcf45195
Andrew Liaw <yhaliaw@example.com>
2022-11-25 15:28:11 +0800

    Execute commands as a microk8s group.

--- files/pre-integration-test.sh
+++ files/pre-integration-test.sh
@@ -9,31 +9,14 @@
 
 # lxd should be install and init by a previous step in integration test action.
 echo "bootstraping lxd juju controller"
-microk8s status --wait-ready
-newgrp microk8s
-juju bootstrap localhost localhost
+/usr/bin/sg microk8s -c microk8s status --wait-ready
+/usr/bin/sg microk8s -c juju bootstrap localhost localhost
 
 echo "Deploying jenkins"
-python3 tests/integration/deploy_jenkins.py
+/usr/bin/sg microk8s -c python3 tests/integration/deploy_jenkins.py
 
 echo "bootstraping microk8s juju controller"
-newgrp microk8s
-juju bootstrap microk8s micro
+/usr/bin/sg microk8s -c juju bootstrap microk8s micro
 juju add-model testing -c micro
 juju switch micro:admin/testing
 sudo microk8s status --wait-ready
-
-echo "Waiting until the outputs files exist"
-until [ -f contorller_name.txt ]
-do
-     sleep 5
-done
-until [ -f model_name.txt ]
-do
-     sleep 5
-done
-until [ -f unit_number.txt ]
-do
-     sleep 5
-done
-echo "Output files found"
