d4b991ac707cac10e3a1999c34393bbb8552e33c
Andrew Liaw <yhaliaw@example.com>
2022-11-25 10:04:46 +0800

    Add back ensuring microk8s as test are failing for this reason.

--- files/pre-integration-test.sh
+++ files/pre-integration-test.sh
@@ -3,6 +3,11 @@
 # Pre-run script for integration test operator-workflows action.
 # https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
 
+# Enure setup of microk8s.
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+newgrp microk8s
+
 # lxd should be install and init by a previous step in integration test action.
 juju bootstrap localhost localhost
 
