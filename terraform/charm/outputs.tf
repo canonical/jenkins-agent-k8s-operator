70041867278838b801ca7811c00d57de8d30f845
charlie4284 <yangsoo.yoon@canonical.com>
2025-09-08 15:07:01 +0000

    fix: output app name

--- terraform/charm/outputs.tf
+++ terraform/charm/outputs.tf
@@ -3,7 +3,7 @@
 
 output "app_name" {
   description = "Name of the deployed application."
-  value       = juju_application.jenkins_k8s.name
+  value       = juju_application.jenkins_agent_k8s.name
 }
 
 output "provides" {
